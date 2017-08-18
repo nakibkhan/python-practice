175838c2e574654c489d51d1eae3876ad8d8072a
Nakib Khan <nakib.khan1988@gmail.com>
2017-08-18 12:22:10 -0500

    Added Excersize for name print

---
+++ Exercises/NameYear100.py
@@ -0,0 +1,15 @@
+import datetime as dt
+from datetime import datetime
+
+name = raw_input("Can you please state your name ? ")
+age = int(input("Can you please state your age ?" ))
+yearsToHundred = 0
+
+if(age < 100):
+    now = datetime.now()
+    yearsToHundred = now.year + (100-age)
+
+
+print "Your name is " + name
+print "You will be a Hundred Years old in %s"\
+      %(yearsToHundred)
