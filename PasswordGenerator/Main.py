9d2a8c6c95d39c072d58885be9ef27558e642575
Nakib Khan <nakib.khan1988@gmail.com>
2017-09-30 14:55:44 -0500

    Created Password Generator taking in length of password desired

---
+++ PasswordGenerator/Main.py
@@ -0,0 +1,12 @@
+from PasswordGenerator import PasswordGenerator
+
+if __name__ == "__main__":
+
+    print("Entering Password Generator")
+    passwordLen = int(input("Please enter the length you would like the password to be"))
+    print("Generating password with length %s" %(passwordLen))
+    print("-------------------------------------------")
+
+    generator = PasswordGenerator(passwordLen)
+    generator.determine()
+
---
+++ PasswordGenerator/PasswordGenerator.py
@@ -0,0 +1,12 @@
+import random
+
+class PasswordGenerator(object):
+    def __init__(self, passwordLen):
+        self.passwordLen = passwordLen
+
+    def determine(self):
+        print("Determining password with Length : %s" %(self.passwordLen))
+        charSet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*())?"
+        password = "".join(random.sample(charSet, self.passwordLen))
+        print("Generated Password %s" %(password))
+        print("--------------------------------------------")
